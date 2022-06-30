babb205eccc743a178d5528f4886e204bab2c8c7
Moin Khan <nurmoin05@gmail.com>
2022-07-01 01:19:49 +0600

    modal updated

--- src/components/BillModal/BillModal.js
+++ src/components/BillModal/BillModal.js
@@ -15,18 +15,13 @@ const BillModal = (props) => {
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header closeButton>
+            <Modal.Header>
                 <Modal.Title id="contained-modal-title-vcenter">
                     Add New Bill Information
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                    consectetur ac, vestibulum at eros.
-                </p>
+                
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="dark" onClick={handleSubmit}>Close</Button>
