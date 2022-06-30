a4c9bb6f172894c7405ff647d5854dbff39654e4
Moin Khan <nurmoin05@gmail.com>
2022-06-30 19:50:30 +0600

    bill table updated

--- src/components/BillTable/BillTable.js
+++ src/components/BillTable/BillTable.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import BillInfo from '../BillInfo/BillInfo';
 
@@ -6,31 +6,36 @@ const BillTable = () => {
     const [billInfo, setBillInfo] = useState([]);
     useEffect(() => {
         fetch('info.json')
-        .then(res => res.json())
-        .then(data => setBillInfo(data))
+            .then(res => res.json())
+            .then(data => setBillInfo(data))
     }, [])
 
     return (
         <div className="BillTable mt-5">
             <div className="inner__billTable container">
                 <div className="billTable__content">
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                            <tr>
-                                <th>Billing Id</th>
-                                <th>Full Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Paid Amount</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                billInfo.map(info => <BillInfo info={info} />)
-                            }
-                        </tbody>
-                    </Table>
+                    <div className="row">
+                        <div className="col-lg-12 col-md-12 col-sm-12">
+                            <Table striped bordered hover variant="dark">
+                                <thead>
+                                    <tr>
+                                        <th>Billing Id</th>
+                                        <th>Full Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Paid Amount</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        billInfo.map(info => <BillInfo info={info} />)
+                                    }
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
