4a903d64bae816f7e8a4db75feb69268632307d4
Moin Khan <nurmoin05@gmail.com>
2022-06-30 17:55:25 +0600

    add new bill modal added

--- src/components/BillHeader/BillHeader.js
+++ src/components/BillHeader/BillHeader.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import BillModal from '../BillModal/BillModal';
 
 const BillHeader = () => {
+    const [modalShow, setModalShow] = useState(false);
+
     return (
         <div className="BillHeader">
             <div className="inner__billheader container">
                 <div className="billheader__content bg-dark p-2">
                     <div className="row row-cols-lg-3 text-center">
                         <div className="bill__heading">
-                            <h3 className="text-white fw-semibold">Billings</h3>
+                            <h4 className="text-white fw-semibold">Billings</h4>
                         </div>
                         <div className="bill__search">
                             <Form>
                                 <Form.Control type="text" placeholder="Search" />
                             </Form>
-                        </div> 
+                        </div>
                         <div className="add__bill">
-                           <Button variant="light" bg="dark">Add New Bill</Button>
+                            <Button onClick={() => setModalShow(true)} variant="light" bg="dark">Add New Bill</Button>
+                            <BillModal show={modalShow} onHide={() => setModalShow(false)} />
                         </div>
                     </div>
                 </div>
